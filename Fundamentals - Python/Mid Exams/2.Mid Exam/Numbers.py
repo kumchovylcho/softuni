5896f31ee802e124928a1a7cb4665fd49b3bef6d
kumchovylcho <tamerchoyy@gmail.com>
2022-08-12 23:11:12 +0300

    finishing the solution

--- Fundamentals - Python/Mid Exams/2.Mid Exam/Numbers.py
+++ Fundamentals - Python/Mid Exams/2.Mid Exam/Numbers.py
@@ -1,10 +1,20 @@
-numbers = input()
+numbers = [int(number) for number in input().split()]
 average_number = sum(numbers) / len(numbers)
 
-top_5_numbers = []
+numbers_higher_than_average_number = [number for number in numbers if number > average_number]
 
-for number in numbers:
-    if len(top_5_numbers) > 5:
-        break
-    if number > average_number:
-        top_5_numbers.append(number)
+numbers_higher_than_average_number.sort()
+
+if len(numbers_higher_than_average_number) >= 5:
+    to_starting_position = len(numbers_higher_than_average_number) - 5
+    del numbers_higher_than_average_number[0:to_starting_position]
+
+numbers_higher_than_average_number.reverse()
+
+if sum(numbers_higher_than_average_number) == 0:
+    print("No")
+
+elif len(numbers_higher_than_average_number) > 0:
+    print(*numbers_higher_than_average_number, sep=" ")
+
+
