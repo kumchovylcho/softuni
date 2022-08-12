d0168f54a9fb742cdef2928008bd134bc7feddcc
kumchovylcho <tamerchoyy@gmail.com>
2022-08-12 23:42:59 +0300

    adding new solution

--- Fundamentals - Python/Mid Exams/2.Mid Exam/Numbers.py
+++ Fundamentals - Python/Mid Exams/2.Mid Exam/Numbers.py
@@ -12,6 +12,35 @@ if len(numbers_higher_than_average_number) == 0:
 
 elif numbers_higher_than_average_number:
     print(*numbers_higher_than_average_number, sep=" ")
-pass
+
+
+
+
+# numbers = [int(number) for number in input().split()]
+# average_number = sum(numbers) / len(numbers)
+#
+# numbers_higher_than_average_number = []
+# [numbers_higher_than_average_number.append(number) for number in numbers if number > average_number]
+#
+# numbers_higher_than_average_number.sort()
+#
+# if len(numbers_higher_than_average_number) >= 5:
+#     to_starting_position = len(numbers_higher_than_average_number) - 5
+#     del numbers_higher_than_average_number[0:to_starting_position]
+#     numbers_higher_than_average_number.reverse()
+#
+#
+# elif len(numbers_higher_than_average_number) < 5:
+#     numbers_higher_than_average_number.reverse()
+#
+#
+# if sum(numbers_higher_than_average_number) == 0:
+#     print("No")
+#
+# elif len(numbers_higher_than_average_number) > 0:
+#     print(*numbers_higher_than_average_number, sep=" ")
+
+
+
 
 
