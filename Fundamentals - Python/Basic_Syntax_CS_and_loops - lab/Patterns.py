443022c90d69d91369114ac95fbb1e6dee7baea2
kumchovylcho <tamerchoyy@gmail.com>
2022-09-14 21:53:45 +0300

    using comprehension

--- Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Patterns.py
+++ Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Patterns.py
@@ -1,9 +1,13 @@
-number_of_max_stars = int(input())
-for star in range(1, number_of_max_stars + 1):
-    for star_backwards in range(0, star):
-        print("*", end="")
-    print()
-for star in range(number_of_max_stars - 1, 0, -1):
-    for star_backwards in range(0, star):
-        print("*", end="")
-    print()
+number = int(input())
+[print(x * "*") for x in range(1, number + 1)]
+[print(x * "*") for x in range(number - 1, 0, -1)]
+
+# number_of_max_stars = int(input())
+# for star in range(1, number_of_max_stars + 1):
+#     for star_backwards in range(0, star):
+#         print("*", end="")
+#     print()
+# for star in range(number_of_max_stars - 1, 0, -1):
+#     for star_backwards in range(0, star):
+#         print("*", end="")
+#     print()
