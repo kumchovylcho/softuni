a89f25852436326d2724bde15d25999d1175d02c
kumchovylcho <tamerchoyy@gmail.com>
2022-09-15 10:41:53 +0300

    using max

--- Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Large_of_three_numbers.py
+++ Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Large_of_three_numbers.py
@@ -1,12 +1,16 @@
-import sys
-first_number = int(input())
-second_number = int(input())
-third_number = int(input())
-largest_number = -sys.maxsize
-if first_number > largest_number:
-    largest_number = first_number
-if second_number > largest_number:
-    largest_number = second_number
-if third_number > largest_number:
-    largest_number = third_number
-print(largest_number)
+first_number, second_number, third_number = int(input()), int(input()), int(input())
+print(max(first_number, second_number, third_number))
+
+
+# import sys
+# first_number = int(input())
+# second_number = int(input())
+# third_number = int(input())
+# largest_number = -sys.maxsize
+# if first_number > largest_number:
+#     largest_number = first_number
+# if second_number > largest_number:
+#     largest_number = second_number
+# if third_number > largest_number:
+#     largest_number = third_number
+# print(largest_number)
