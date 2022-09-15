de84b8371b7e48d0ac5a4a82e0bf306f13bf343a
kumchovylcho <tamerchoyy@gmail.com>
2022-09-15 10:42:23 +0300

    ref

--- Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Number_between_1_and_100.py
+++ Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Number_between_1_and_100.py
@@ -1,4 +1,8 @@
 number = float(input())
-while 1 > number or number > 100:
+while 1 > number < 100:
     number = float(input())
-print(f"The number {number} is between 1 and 100")
+else:
+    print(f"The number {number} is between 1 and 100")
+
+# if you break the loop using break , it will not enter this else statement
+# if the while loop condition returns False without using break , else statement will be executed
