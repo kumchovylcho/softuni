c023271f052801cfabfb6ae5a2790eda468cd596
kumchovylcho <tamerchoyy@gmail.com>
2022-09-15 10:44:04 +0300

    ref

--- Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Word_reverse.py
+++ Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Word_reverse.py
@@ -1,5 +1,11 @@
-word = input()
-reversed_word = ""
-for letters in range(len(word) - 1, -1, - 1):
-    reversed_word += word[letters]
-print(reversed_word)
+print(input()[::-1])
+
+# text = input()
+# print(text[::-1])
+
+
+# word = input()
+# reversed_word = ""
+# for letters in range(len(word) - 1, -1, - 1):
+#     reversed_word += word[letters]
+# print(reversed_word)
