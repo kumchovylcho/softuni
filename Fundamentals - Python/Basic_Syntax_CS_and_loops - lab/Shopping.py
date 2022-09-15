4a3f6ca26ec56f4fde7fcda65867ed9bb398059c
kumchovylcho <tamerchoyy@gmail.com>
2022-09-15 10:43:35 +0300

    ref

--- Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Shopping.py
+++ Fundamentals - Python/Basic_Syntax_CS_and_loops - lab/Shopping.py
@@ -1,13 +1,26 @@
 budget = int(input())
-money_left = budget
-command = input()
-while command != "End":
-    product_price = int(command)
-    money_left -= product_price
-    if money_left < 0:
+product_price = input()
+while product_price != "End":
+    product_price = int(product_price)
+    if budget < product_price:
+        print("You went in overdraft!")
         break
-    command = input()
-if money_left < 0:
-    print("You went in overdraft!")
+    budget -= product_price
+    product_price = input()
 else:
     print("You bought everything needed.")
+
+
+# budget = int(input())
+# money_left = budget
+# command = input()
+# while command != "End":
+#     product_price = int(command)
+#     money_left -= product_price
+#     if money_left < 0:
+#         break
+#     command = input()
+# if money_left < 0:
+#     print("You went in overdraft!")
+# else:
+#     print("You bought everything needed.")
