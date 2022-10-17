785e8865c2dbd2516a52e522512f3d98255734e8
kumchovylcho <tamerchoyy@gmail.com>
2022-10-17 23:33:03 +0300

    ref

--- Fundamentals - Python/Mid Exams/1.Mid Exam/Computer_store.py
+++ Fundamentals - Python/Mid Exams/1.Mid Exam/Computer_store.py
@@ -1,26 +1,49 @@
-total_price_without_taxes = 0
-all_taxes = 0
-part_prices = input()
-while part_prices != "special" and part_prices != "regular":
-    part_prices = float(part_prices)
-    if part_prices < 0:
+part_prices_without_tax = 0
+command = input()
+while command != "special" and command != "regular":
+    current_part_price = float(command)
+    if current_part_price < 0:
         print("Invalid price!")
-        part_prices = input()
+        command = input()
         continue
-    total_price_without_taxes += part_prices
-    all_taxes += part_prices * 0.2
-    part_prices = input()
-
-total_price = total_price_without_taxes + all_taxes
-if part_prices == "special":
-    total_price *= 0.9
-
-if total_price == 0:
+    part_prices_without_tax += current_part_price
+    command = input()
+taxes = part_prices_without_tax * 0.2
+total_price = part_prices_without_tax + (part_prices_without_tax * 0.2)
+if not part_prices_without_tax:
     print("Invalid order!")
-
-elif total_price:
-    print("Congratulations you've just bought a new computer!")
-    print(f"Price without taxes: {total_price_without_taxes:.2f}$")
-    print(f"Taxes: {all_taxes:.2f}$\n-----------")
+if command == "special":
+    total_price *= 0.9
+if (command == "special" or command == "regular") and part_prices_without_tax:
+    print(f"Congratulations you've just bought a new computer!")
+    print(f"Price without taxes: {part_prices_without_tax:.2f}$")
+    print(f"Taxes: {taxes:.2f}$\n-----------")
     print(f"Total price: {total_price:.2f}$")
 
+
+# total_price_without_taxes = 0
+# all_taxes = 0
+# part_prices_without_tax = input()
+# while part_prices_without_tax != "special" and part_prices_without_tax != "regular":
+#     part_prices_without_tax = float(part_prices_without_tax)
+#     if part_prices_without_tax < 0:
+#         print("Invalid price!")
+#         part_prices_without_tax = input()
+#         continue
+#     total_price_without_taxes += part_prices_without_tax
+#     all_taxes += part_prices_without_tax * 0.2
+#     part_prices_without_tax = input()
+#
+# total_price = total_price_without_taxes + all_taxes
+# if part_prices_without_tax == "special":
+#     total_price *= 0.9
+#
+# if total_price == 0:
+#     print("Invalid order!")
+#
+# elif total_price:
+#     print("Congratulations you've just bought a new computer!")
+#     print(f"Price without taxes: {total_price_without_taxes:.2f}$")
+#     print(f"Taxes: {all_taxes:.2f}$\n-----------")
+#     print(f"Total price: {total_price:.2f}$")
+#
--- Fundamentals - Python/Mid Exams/1.Mid Exam/The_lift.py
+++ Fundamentals - Python/Mid Exams/1.Mid Exam/The_lift.py
@@ -1,31 +1,56 @@
-que_for_the_lift = int(input())
-state_of_the_lift = [int(lift) for lift in input().split()]
+def add_people(all_lifts_list, index, people_remaining):
+    max_people_per_lift = 4
+    people_added = max_people_per_lift - all_lifts_list[index]
+    if people_remaining >= people_added:
+        all_lifts_list[index] += people_added
+        people_remaining -= people_added
+    elif people_remaining < people_added:
+        all_lifts_list[index] += people_remaining
+        people_remaining = 0
+    return all_lifts_list, people_remaining
 
 
-def space_on_lift(current_lift, taken_seats):
-    global que_for_the_lift
-    free_spots = 4 - taken_seats
-    if free_spots <= que_for_the_lift:
-        state_of_the_lift[current_lift] = taken_seats + free_spots
-        que_for_the_lift -= free_spots
-    elif free_spots > que_for_the_lift:
-        state_of_the_lift[current_lift] += que_for_the_lift
-        que_for_the_lift -= que_for_the_lift
-
-
-for lift_position, busy_seats in enumerate(state_of_the_lift):
-    if busy_seats < 4:
-        space_on_lift(lift_position, busy_seats)
-
-
-empty_spots = [spot for spot in state_of_the_lift if spot < 4]
-if not empty_spots and que_for_the_lift == 0:
-    print(*state_of_the_lift, end=" ")
-
-elif empty_spots:
+que_of_people = int(input())
+state_of_lifts = list(map(int, input().split()))
+for lift in range(len(state_of_lifts)):
+    if state_of_lifts[lift] < 4:
+        state_of_lifts, que_of_people = add_people(state_of_lifts, lift, que_of_people)
+check_for_empty_spots = [lift for lift in state_of_lifts if lift != 4]
+if check_for_empty_spots:
     print("The lift has empty spots!")
-    print(*state_of_the_lift, end=" ")
+elif que_of_people:
+    print(f"There isn't enough space! {que_of_people} people in a queue!")
+print(' '.join(str(lift) for lift in state_of_lifts))
 
-elif not empty_spots:
-    print(f"There isn't enough space! {que_for_the_lift} people in a queue!")
-    print(*state_of_the_lift, end=" ")
+
+# que_for_the_lift = int(input())
+# state_of_the_lift = [int(lift) for lift in input().split()]
+#
+#
+# def space_on_lift(current_lift, taken_seats):
+#     global que_for_the_lift
+#     free_spots = 4 - taken_seats
+#     if free_spots <= que_for_the_lift:
+#         state_of_the_lift[current_lift] = taken_seats + free_spots
+#         que_for_the_lift -= free_spots
+#     elif free_spots > que_for_the_lift:
+#         state_of_the_lift[current_lift] += que_for_the_lift
+#         que_for_the_lift -= que_for_the_lift
+#
+#
+# for lift_position, busy_seats in enumerate(state_of_the_lift):
+#     if busy_seats < 4:
+#         space_on_lift(lift_position, busy_seats)
+#
+#
+# empty_spots = [spot for spot in state_of_the_lift if spot < 4]
+# if not empty_spots and que_for_the_lift == 0:
+#     print(*state_of_the_lift, end=" ")
+#
+# elif empty_spots:
+#     print("The lift has empty spots!")
+#     print(*state_of_the_lift, end=" ")
+#
+# elif not empty_spots:
+#     print(f"There isn't enough space! {que_for_the_lift} people in a queue!")
+#     print(*state_of_the_lift, end=" ")
