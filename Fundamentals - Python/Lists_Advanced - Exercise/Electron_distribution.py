74a8ec0fd24f065aee6b04fc767841e35a952f48
kumchovylcho <tamerchoyy@gmail.com>
2022-10-20 00:46:01 +0300

    ref

--- Fundamentals - Python/Lists_Advanced - Exercise/Electron_distribution.py
+++ Fundamentals - Python/Lists_Advanced - Exercise/Electron_distribution.py
@@ -1,15 +1,27 @@
-electrons = int(input())
-list_with_filled_shells = []
+number = int(input())
+n = 1
+lst = []
+while number > 0:
+    electron = 2*n**2
+    lst.append(min(number, electron))
+    number -= lst[-1]
+    n += 1
 
-for electron in range(1, electrons + 1):
-    result = 2 * (electron ** 2)
-    if result > electrons:
-        list_with_filled_shells.append(electrons)
-        electrons -= electrons
-        break
-    electrons -= result
-    list_with_filled_shells.append(result)
-    if electrons <= 0:
-        break
+print(lst)
 
-print(list_with_filled_shells)
+
+# electrons = int(input())
+# list_with_filled_shells = []
+#
+# for electron in range(1, electrons + 1):
+#     result = 2 * (electron ** 2)
+#     if result > electrons:
+#         list_with_filled_shells.append(electrons)
+#         electrons -= electrons
+#         break
+#     electrons -= result
+#     list_with_filled_shells.append(result)
+#     if electrons <= 0:
+#         break
+#
+# print(list_with_filled_shells)
