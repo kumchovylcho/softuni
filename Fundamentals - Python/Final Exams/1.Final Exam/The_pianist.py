56f85fcc00f2e52319b81450dd49365f69d7d740
kumchovylcho <tamerchoyy@gmail.com>
2022-10-29 13:35:12 +0300

    ref

--- Fundamentals - Python/Final Exams/1.Final Exam/The_pianist.py
+++ Fundamentals - Python/Final Exams/1.Final Exam/The_pianist.py
@@ -1,45 +1,90 @@
+def add_command(piece, composer, key):
+    if piece in piece_info:
+        print(f"{piece} is already in the collection!")
+    else:
+        piece_info[piece] = {'composer': composer, 'key': key}
+        print(f"{piece} by {composer} in {key} added to the collection!")
+
+
+def remove_command(piece):
+    if piece in piece_info:
+        del piece_info[piece]
+        print(f"Successfully removed {piece}!")
+    else:
+        print(f"Invalid operation! {piece} does not exist in the collection.")
+
+
+def change_key_command(piece, new_key):
+    if piece in piece_info:
+        piece_info[piece]['key'] = new_key
+        print(f"Changed the key of {piece} to {new_key}!")
+    else:
+        print(f"Invalid operation! {piece} does not exist in the collection.")
+
+
 number_of_pieces = int(input())
-pianist_collection = {}
+piece_info = {}
 
-for pieces in range(number_of_pieces):
+for _ in range(number_of_pieces):
     piece, composer, key = input().split("|")
-    pianist_collection[piece] = pianist_collection.get(composer, {})
-    pianist_collection[piece][composer] = pianist_collection[piece].get(composer, key)
+    piece_info[piece] = {'composer': composer, 'key': key}
 
-command = input()
-while command != "Stop":
-    command = command.split("|")
-    operation = command[0]
-    piece = command[1]
+command_line = input()
+while not command_line == 'Stop':
+    command, *info = [item for item in command_line.split("|")]
+    if command == 'Add':
+        add_command(*info)
+    elif command == 'Remove':
+        remove_command(*info)
+    elif command == 'ChangeKey':
+        change_key_command(*info)
+    command_line = input()
+for piece, info in piece_info.items():
+    print(f"{piece} -> Composer: {info['composer']}, Key: {info['key']}")
 
-    if operation == "Add":
-        composer, key = command[2], command[3]
-        if piece not in pianist_collection:
-            pianist_collection[piece] = {composer: key}
-            print(f"{piece} by {composer} in {key} added to the collection!")
-        else:
-            print(f"{piece} is already in the collection!")
 
-    elif operation == "Remove":
-        if piece in pianist_collection:
-            del pianist_collection[piece]
-            print(f"Successfully removed {piece}!")
-        else:
-            print(f"Invalid operation! {piece} does not exist in the collection.")
-
-    elif operation == "ChangeKey":
-        new_key = command[2]
-        if piece in pianist_collection:
-            for current_key in pianist_collection:
-                if current_key == piece:
-                    for compositor in pianist_collection[current_key]:
-                        pianist_collection[current_key][compositor] = new_key
-                        break
-            print(f"Changed the key of {piece} to {new_key}!")
-        else:
-            print(f"Invalid operation! {piece} does not exist in the collection.")
-    command = input()
-
-for keys in pianist_collection:
-    for composers in pianist_collection[keys]:
-        print(f"{keys} -> Composer: {composers}, Key: {pianist_collection[keys][composers]}")
+# number_of_pieces = int(input())
+# pianist_collection = {}
+#
+# for pieces in range(number_of_pieces):
+#     piece, composer, key = input().split("|")
+#     pianist_collection[piece] = pianist_collection.get(composer, {})
+#     pianist_collection[piece][composer] = pianist_collection[piece].get(composer, key)
+#
+# command = input()
+# while command != "Stop":
+#     command = command.split("|")
+#     operation = command[0]
+#     piece = command[1]
+#
+#     if operation == "Add":
+#         composer, key = command[2], command[3]
+#         if piece not in pianist_collection:
+#             pianist_collection[piece] = {composer: key}
+#             print(f"{piece} by {composer} in {key} added to the collection!")
+#         else:
+#             print(f"{piece} is already in the collection!")
+#
+#     elif operation == "Remove":
+#         if piece in pianist_collection:
+#             del pianist_collection[piece]
+#             print(f"Successfully removed {piece}!")
+#         else:
+#             print(f"Invalid operation! {piece} does not exist in the collection.")
+#
+#     elif operation == "ChangeKey":
+#         new_key = command[2]
+#         if piece in pianist_collection:
+#             for current_key in pianist_collection:
+#                 if current_key == piece:
+#                     for compositor in pianist_collection[current_key]:
+#                         pianist_collection[current_key][compositor] = new_key
+#                         break
+#             print(f"Changed the key of {piece} to {new_key}!")
+#         else:
+#             print(f"Invalid operation! {piece} does not exist in the collection.")
+#     command = input()
+#
+# for keys in pianist_collection:
+#     for composers in pianist_collection[keys]:
+#         print(f"{keys} -> Composer: {composers}, Key: {pianist_collection[keys][composers]}")
