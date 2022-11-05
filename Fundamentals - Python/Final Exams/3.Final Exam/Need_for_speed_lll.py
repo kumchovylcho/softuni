d76c3080ccb3544bac057d043d2d90596a9acb17
kumchovylcho <tamerchoyy@gmail.com>
2022-11-05 23:15:46 +0200

    ref

--- Fundamentals - Python/Final Exams/3.Final Exam/Need_for_speed_lll.py
+++ Fundamentals - Python/Final Exams/3.Final Exam/Need_for_speed_lll.py
@@ -60,6 +60,14 @@ for car in range(number_of_cars):
     cars_information[current_car] = cars_information.get(current_car, {'mileage': mileage, 'fuel': fuel})
 
 
+# number_of_cars = int(input())
+# cars_information = {}
+#
+# for car in range(number_of_cars):
+#     current_car, mileage, fuel = [string if string[0].isalpha() else int(string) for string in input().split("|")]
+#     cars_information[current_car] = cars_information.get(current_car, {'mileage': mileage, 'fuel': fuel})
+#
+#
 # command = input()
 # while command != "Stop":
 #     command = command.split(" : ")
@@ -99,5 +107,6 @@ for car in range(number_of_cars):
 #     command = input()
 #
 # for remaining_cars in cars_information:
-#     print(f"{remaining_cars} -> Mileage: {cars_information[remaining_cars]['mileage']} kms, "
-#           f"Fuel in the tank: {cars_information[remaining_cars]['fuel']} lt.")
+#     print(f"{remaining_cars} -> Mileage: {cars_information[remaining_cars]['mileage']} kms, Fuel in the tank: "
+#           f"{cars_information[remaining_cars]['fuel']} lt.")
+#
