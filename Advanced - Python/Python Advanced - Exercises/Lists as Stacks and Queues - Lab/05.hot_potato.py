0ae39a7e02b8bc5c9bff2485cb947966c1b63876
kumchovylcho <tamerchoyy@gmail.com>
2022-11-12 01:06:08 +0200

    ref

--- Advanced - Python/Python Advanced - Exercises/Lists as Stacks and Queues - Lab/05.hot_potato.py
+++ Advanced - Python/Python Advanced - Exercises/Lists as Stacks and Queues - Lab/05.hot_potato.py
@@ -1,24 +1,7 @@
-# from collections import deque
-# kids = deque(input().split())
-# skips = int(input())
-# while len(kids) > 1:
-#     kids.rotate(-skips)
-#     print(f"Removed {kids.pop()}")
-# print(f"Last is {''.join(kids)}")
-
-
-kids = input().split()
+from collections import deque
+kids = deque(input().split())
 skips = int(input())
-index = 0
-counter = 1
 while len(kids) > 1:
-    if 0 <= index < len(kids):
-        if counter % skips == 0:
-            print(f"Removed {kids.pop(index)}")
-    else:
-        index = 0
-        counter += 1
-        continue
-
-    counter += 1
-    index += 1
+    kids.rotate(-skips)
+    print(f"Removed {kids.pop()}")
+print(f"Last is {''.join(kids)}")
