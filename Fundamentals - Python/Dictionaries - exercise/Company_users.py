0c01199e334770b3d03388cad73fc6c8b6ec056d
kumchovylcho <tamerchoyy@gmail.com>
2022-11-13 02:16:57 +0200

    ref

--- Fundamentals - Python/Dictionaries - exercise/Company_users.py
+++ Fundamentals - Python/Dictionaries - exercise/Company_users.py
@@ -1,19 +1,44 @@
-company_info = {}
+class Company:
+    def __init__(self):
+        self.company = {}
 
-company_employee = input()
-while company_employee != "End":
-    company_employee = company_employee.split(" -> ")
-    company, employee = company_employee[0], company_employee[1]
+    def check_for_company(self, company_name):
+        self.company[company_name] = self.company.get(company_name, [])
 
-    company_info[company] = company_info.get(company, [])
-    if employee not in company_info[company]:
-        company_info[company].append(employee)
+    def check_for_user(self, user, company_name):
+        if user not in self.company[company_name]:
+            self.company[company_name].append(user)
 
-    company_employee = input()
 
-for key in company_info:
+company_users = Company()
+current_company = input()
+while current_company != "End":
+    name_of_company, employee_id = current_company.split(" -> ")
+    company_users.check_for_company(name_of_company)
+    company_users.check_for_user(employee_id, name_of_company)
+    current_company = input()
+for key in company_users.company:
     print(key)
-    [print(f"-- {employee}") for employee in company_info[key]]
+    for user_id in company_users.company[key]:
+        print(f"-- {user_id}")
+
+
+# company_info = {}
+#
+# company_employee = input()
+# while company_employee != "End":
+#     company_employee = company_employee.split(" -> ")
+#     company, employee = company_employee[0], company_employee[1]
+#
+#     company_info[company] = company_info.get(company, [])
+#     if employee not in company_info[company]:
+#         company_info[company].append(employee)
+#
+#     company_employee = input()
+#
+# for key in company_info:
+#     print(key)
+#     [print(f"-- {employee}") for employee in company_info[key]]
 
 
 # company_info = dict()
