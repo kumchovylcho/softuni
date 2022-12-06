751d5e2f7e423b67862270dc6707b0bc72029c32
kumchovylcho <tamerchoyy@gmail.com>
2022-12-06 22:29:01 +0200

    ref

--- Fundamentals - Python/Mid Exams/3.Mid Exam/Moving_target.py
+++ Fundamentals - Python/Mid Exams/3.Mid Exam/Moving_target.py
@@ -1,41 +1,81 @@
-def shooting(position, strike_damage):
-    if 0 <= position < len(targets):
-        targets[position] -= strike_damage
-        if targets[position] <= 0:
-            targets.pop(position)
+def shoot(current_index, power, current_targets):
+    if 0 <= current_index < len(current_targets):
+        current_targets[current_index] -= power
+        if current_targets[current_index] <= 0:
+            current_targets.pop(current_index)
+    return current_targets
 
 
-def add_target(position, target_value):
-    if 0 <= position < len(targets):
-        targets.insert(position, target_value)
-    elif position >= len(targets) or position < 0:
+def add(current_index, add_value, current_targets):
+    if 0 <= current_index < len(current_targets):
+        current_targets.insert(current_index, add_value)
+    else:
         print("Invalid placement!")
+    return current_targets
 
 
-def strike(position, radius_of_strike):
-    from_current_position_to_0 = position - radius_of_strike     # if the result is less than zero, strike will miss
-    from_current_position_to_len_targets = position + radius_of_strike  # if the result is more than len(targets) , strike will miss
-    if from_current_position_to_0 >= 0 and from_current_position_to_len_targets < len(targets):
-        del targets[from_current_position_to_0:from_current_position_to_len_targets + 1]
-    elif from_current_position_to_0 < 0 or from_current_position_to_len_targets >= len(targets):
+def strike(current_index, radius, current_targets):
+    start_index = current_index - radius
+    end_index = current_index + radius
+    if 0 <= start_index < len(current_targets) and 0 <= end_index < len(current_targets):
+        current_targets = current_targets[:start_index] + current_targets[end_index + 1:]
+    else:
         print("Strike missed!")
+    return current_targets
 
 
 targets = [int(target) for target in input().split()]
+call_functions = {
+    "Shoot": shoot,
+    "Add": add,
+    "Strike": strike
+}
 command = input()
 while command != "End":
-    command = command.split()
-    event = command[0]
-    index = int(command[1])
-    if event == "Shoot":
-        strike_power = int(command[2])
-        shooting(index, strike_power)
-    elif event == "Add":
-        new_target = int(command[2])
-        add_target(index, new_target)
-    elif event == "Strike":
-        radius_of_the_strike = int(command[2])
-        strike(index, radius_of_the_strike)
+    task, index, info = [x if x.isalpha() else int(x) for x in command.split()]
+    targets = call_functions[task](index, info, targets)
     command = input()
+print(*targets, sep='|')
 
-print(*targets, sep="|")
+
+# def shooting(position, strike_damage):
+#     if 0 <= position < len(targets):
+#         targets[position] -= strike_damage
+#         if targets[position] <= 0:
+#             targets.pop(position)
+#
+#
+# def add_target(position, target_value):
+#     if 0 <= position < len(targets):
+#         targets.insert(position, target_value)
+#     elif position >= len(targets) or position < 0:
+#         print("Invalid placement!")
+#
+#
+# def strike(position, radius_of_strike):
+#     from_current_position_to_0 = position - radius_of_strike     # if the result is less than zero, strike will miss
+#     from_current_position_to_len_targets = position + radius_of_strike  # if the result is more than len(targets) , strike will miss
+#     if from_current_position_to_0 >= 0 and from_current_position_to_len_targets < len(targets):
+#         del targets[from_current_position_to_0:from_current_position_to_len_targets + 1]
+#     elif from_current_position_to_0 < 0 or from_current_position_to_len_targets >= len(targets):
+#         print("Strike missed!")
+#
+#
+# targets = [int(target) for target in input().split()]
+# command = input()
+# while command != "End":
+#     command = command.split()
+#     event = command[0]
+#     index = int(command[1])
+#     if event == "Shoot":
+#         strike_power = int(command[2])
+#         shooting(index, strike_power)
+#     elif event == "Add":
+#         new_target = int(command[2])
+#         add_target(index, new_target)
+#     elif event == "Strike":
+#         radius_of_the_strike = int(command[2])
+#         strike(index, radius_of_the_strike)
+#     command = input()
+#
+# print(*targets, sep="|")
--- Fundamentals - Python/Mid Exams/4.Mid Exam/Heart_delivery.py
+++ Fundamentals - Python/Mid Exams/4.Mid Exam/Heart_delivery.py
@@ -1,34 +1,56 @@
-def jump_on_houses(current_jump, last_house_jumped):
-    last_house_jumped += current_jump
-    if last_house_jumped >= len(neighborhood):
-        last_house_jumped = 0
-    if 0 <= last_house_jumped < len(neighborhood):
-        if neighborhood[last_house_jumped] >= 2:
-            neighborhood[last_house_jumped] -= 2
-            if neighborhood[last_house_jumped] == 0:
-                print(f"Place {last_house_jumped} has Valentine's day.")
-        elif neighborhood[last_house_jumped] == 0:
-            print(f"Place {last_house_jumped} already had Valentine's day.")
-    return last_house_jumped
-
-
-neighborhood = [int(num) for num in input().split("@")]
-last_house = 0
-jump = input()
-while jump != "Love!":
-    jump = jump.split()
-    jump = int(jump[1])
-    last_house = jump_on_houses(jump, last_house)
-    jump = input()
-
-print(f"Cupid's last position was {last_house}.")
-
-cupid_fails = 0
-for hearts in neighborhood:
-    if hearts != 0:
-        cupid_fails += 1
-
-if not cupid_fails:
+neighborhood = [int(x) for x in input().split("@")]
+cupid_house = 0
+current_jump = input()
+while current_jump != "Love!":
+    _, jump_power = [int(x) if x.isdigit() else x for x in current_jump.split()]
+    cupid_house += jump_power
+    if cupid_house >= len(neighborhood):
+        cupid_house = 0
+    if neighborhood[cupid_house] > 0:
+        neighborhood[cupid_house] -= 2
+        if neighborhood[cupid_house] == 0:
+            print(f"Place {cupid_house} has Valentine's day.")
+    elif neighborhood[cupid_house] == 0:
+        print(f"Place {cupid_house} already had Valentine's day.")
+    current_jump = input()
+print(f"Cupid's last position was {cupid_house}.")
+if all(True if house == 0 else False for house in neighborhood):
     print("Mission was successful.")
-elif cupid_fails:
-    print(f"Cupid has failed {cupid_fails} places.")
+elif any(True if house > 0 else False for house in neighborhood):
+    print(f"Cupid has failed {len(neighborhood) - neighborhood.count(0)} places.")
+
+
+# def jump_on_houses(current_jump, last_house_jumped):
+#     last_house_jumped += current_jump
+#     if last_house_jumped >= len(neighborhood):
+#         last_house_jumped = 0
+#     if 0 <= last_house_jumped < len(neighborhood):
+#         if neighborhood[last_house_jumped] >= 2:
+#             neighborhood[last_house_jumped] -= 2
+#             if neighborhood[last_house_jumped] == 0:
+#                 print(f"Place {last_house_jumped} has Valentine's day.")
+#         elif neighborhood[last_house_jumped] == 0:
+#             print(f"Place {last_house_jumped} already had Valentine's day.")
+#     return last_house_jumped
+#
+#
+# neighborhood = [int(num) for num in input().split("@")]
+# last_house = 0
+# jump = input()
+# while jump != "Love!":
+#     jump = jump.split()
+#     jump = int(jump[1])
+#     last_house = jump_on_houses(jump, last_house)
+#     jump = input()
+#
+# print(f"Cupid's last position was {last_house}.")
+#
+# cupid_fails = 0
+# for hearts in neighborhood:
+#     if hearts != 0:
+#         cupid_fails += 1
+#
+# if not cupid_fails:
+#     print("Mission was successful.")
+# elif cupid_fails:
+#     print(f"Cupid has failed {cupid_fails} places.")
