5a10d289782adc274e730ad8b37e623dbf2f659a
kumchovylcho <tamerchoyy@gmail.com>
2022-12-25 00:29:48 +0200

    initial solution

--- Advanced - Python/Python Advanced - Exercises/Stacks, Queues, Tuples and Sets - Exercise/06.paint_colors.py
+++ Advanced - Python/Python Advanced - Exercises/Stacks, Queues, Tuples and Sets - Exercise/06.paint_colors.py
@@ -37,16 +37,10 @@ while colors_given:
             if second_part:
                 colors_given.insert(mid + 1, second_part)
         continue
-    if color_name in main_colors:
+    if color_name in main_colors or color_name in secondary_colors:
         result.append(color_name)
         continue
-    if color_name_backwards in main_colors:
-        result.append(color_name_backwards)
-        continue
-    if color_name in secondary_colors:
-        result.append(color_name)
-        continue
-    if color_name_backwards in secondary_colors:
+    if color_name_backwards in main_colors or color_name_backwards in secondary_colors:
         result.append(color_name_backwards)
 all_colors = ("red", "yellow", "blue", "orange", "purple", "green")
 for color, requirement_colors in secondary_colors.items():
@@ -55,40 +49,3 @@ for color, requirement_colors in secondary_colors.items():
             result.remove(color)
             break
 print(result)
-
-
-
-# from collections import deque
-#
-# substring = deque(input().split())
-#
-# colors = {
-#     "all colors": ("red", "yellow", "blue", "orange", "purple", "green"),
-#     "required colors": {
-#         "orange": ("red", "yellow"),
-#         "purple": ("red", "blue"),
-#         "green": ("yellow", "blue")
-#     }
-# }
-#
-# result = []
-#
-# while substring:
-#     last_part = ""
-#     if len(substring) > 1:
-#         last_part = substring.pop()
-#     first_part = substring.popleft()
-#     for color in (first_part + last_part, last_part + first_part):
-#         if color in colors["all colors"]:
-#             result.append(color)
-#             break
-#     else:
-#         for item in (first_part[:-1], last_part[:-1]):
-#             if item:
-#                 substring.insert(len(substring) // 2, item)
-#
-# for color, req_colors in colors["required colors"].items():
-#     if any(x not in result and color in result for x in req_colors):
-#         result.remove(color)
-#
-# print(result)
