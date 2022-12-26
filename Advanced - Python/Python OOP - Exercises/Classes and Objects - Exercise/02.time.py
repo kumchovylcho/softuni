df0ee6c09cf718212121742e115b6a2885fb79c2
kumchovylcho <tamerchoyy@gmail.com>
2022-12-27 01:35:59 +0200

    ref

--- Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/02.time.py
+++ Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/02.time.py
@@ -9,20 +9,53 @@ class Time:
         self.seconds = int(seconds)
 
     def set_time(self, hours: int, minutes: int, seconds: int):
-        self.hours, self.minutes, self.seconds = int(hours), int(minutes), int(seconds)
+        self.hours, self.minutes, self.seconds = hours, minutes, seconds
 
     def get_time(self):
-        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
 
     def next_second(self):
         self.seconds += 1
         if self.seconds > Time.max_seconds:
             self.seconds = 0
             self.minutes += 1
-            if self.minutes > Time.max_minutes:
-                self.minutes = 0
-                self.hours += 1
-                if self.hours > Time.max_hours:
-                    self.hours = 0
+        if self.minutes > Time.max_minutes:
+            self.minutes = 0
+            self.hours += 1
+        if self.hours > Time.max_hours:
+            self.hours = 0
         return self.get_time()
+
+
+time = Time(10, 59, 59)
+print(time.next_second())
+
+
+# class Time:
+#     max_hours = 23
+#     max_minutes = 59
+#     max_seconds = 59
+#
+#     def __init__(self, hours: int, minutes: int, seconds: int):
+#         self.hours = int(hours)
+#         self.minutes = int(minutes)
+#         self.seconds = int(seconds)
+#
+#     def set_time(self, hours: int, minutes: int, seconds: int):
+#         self.hours, self.minutes, self.seconds = int(hours), int(minutes), int(seconds)
+#
+#     def get_time(self):
+#         return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
+#
+#     def next_second(self):
+#         self.seconds += 1
+#         if self.seconds > Time.max_seconds:
+#             self.seconds = 0
+#             self.minutes += 1
+#             if self.minutes > Time.max_minutes:
+#                 self.minutes = 0
+#                 self.hours += 1
+#                 if self.hours > Time.max_hours:
+#                     self.hours = 0
+#         return self.get_time()
 
