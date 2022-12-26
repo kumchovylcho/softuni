5f7ae57ecced6cfa0d7eee750ca1872642f74821
kumchovylcho <tamerchoyy@gmail.com>
2022-12-26 02:04:44 +0200

    ref

--- Advanced - Python/Python OOP - Exercises/Classes and Objects - Lab/04.Glass.py
+++ Advanced - Python/Python OOP - Exercises/Classes and Objects - Lab/04.Glass.py
@@ -1,9 +1,11 @@
 class Glass:
-    content = 0
     capacity = 250
 
-    def fill(self, fill_amount):
-        if self.content + fill_amount <= self.capacity:
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, fill_amount: int):
+        if self.content + fill_amount <= Glass.capacity:
             self.content += fill_amount
             return f"Glass filled with {fill_amount} ml"
         return f"Cannot add {fill_amount} ml"
@@ -13,4 +15,23 @@ class Glass:
         return "Glass is now empty"
 
     def info(self):
-        return f"{self.capacity - self.content} ml left"
+        space_left = Glass.capacity - self.content
+        return f"{space_left} ml left"
+
+
+# class Glass:
+#     content = 0
+#     capacity = 250
+#
+#     def fill(self, fill_amount):
+#         if self.content + fill_amount <= self.capacity:
+#             self.content += fill_amount
+#             return f"Glass filled with {fill_amount} ml"
+#         return f"Cannot add {fill_amount} ml"
+#
+#     def empty(self):
+#         self.content = 0
+#         return "Glass is now empty"
+#
+#     def info(self):
+#         return f"{self.capacity - self.content} ml left"
--- Advanced - Python/Python OOP - Exercises/First Steps in OOP - Exercise/05.flower.py
+++ Advanced - Python/Python OOP - Exercises/First Steps in OOP - Exercise/05.flower.py
@@ -1,15 +1,32 @@
 class Flower:
-    is_happy = False
 
-    def __init__(self, name, water_requirements: int):
+    def __init__(self, name: str, water_requirements: int):
         self.name = name
         self.water_requirements = int(water_requirements)
+        self.is_happy = False
 
-    def water(self, water_amount):
-        if water_amount >= self.water_requirements:
-            Flower.is_happy = True
+    def water(self, water_quantity: int):
+        if water_quantity >= self.water_requirements:
+            self.is_happy = True
 
     def status(self):
-        if Flower.is_happy:
+        if self.is_happy:
             return f"{self.name} is happy"
         return f"{self.name} is not happy"
+
+
+# class Flower:
+#     is_happy = False
+#
+#     def __init__(self, name, water_requirements: int):
+#         self.name = name
+#         self.water_requirements = int(water_requirements)
+#
+#     def water(self, water_amount):
+#         if water_amount >= self.water_requirements:
+#             Flower.is_happy = True
+#
+#     def status(self):
+#         if Flower.is_happy:
+#             return f"{self.name} is happy"
+#         return f"{self.name} is not happy"
