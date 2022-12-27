9513f59058db668e5f0e5b1342524a3c46e75eef
kumchovylcho <tamerchoyy@gmail.com>
2022-12-28 00:01:11 +0200

    ref

--- Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/04.pizza_delivery.py
+++ Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/04.pizza_delivery.py
@@ -1,6 +1,6 @@
 class PizzaDelivery:
 
-    def __init__(self, name, price: float, ingredients: dict):
+    def __init__(self, name: str, price: float, ingredients: dict):
         self.name = name
         self.price = float(price)
         self.ingredients = ingredients
@@ -9,29 +9,75 @@ class PizzaDelivery:
     def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
         if self.ordered:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
-        if ingredient in self.ingredients:
-            self.ingredients[ingredient] += int(quantity)
-            self.price += float(price_per_quantity) * int(quantity)
-        elif ingredient not in self.ingredients:
-            self.ingredients[ingredient] = int(quantity)
-            self.price += int(quantity) * float(price_per_quantity)
+        self.ingredients[ingredient] = self.ingredients.get(ingredient, 0) + quantity
+        # if ingredient in self.ingredients:
+        #     self.ingredients[ingredient] += quantity
+        # elif ingredient not in self.ingredients:
+        #     self.ingredients[ingredient] = quantity
+        self.price += quantity * price_per_quantity
 
     def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
         if self.ordered:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
         if ingredient not in self.ingredients:
             return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
-        elif ingredient in self.ingredients and self.ingredients[ingredient] < quantity:
+        if self.ingredients[ingredient] < quantity:
             return f"Please check again the desired quantity of {ingredient}!"
-        self.ingredients[ingredient] -= int(quantity)
-        self.price -= float(price_per_quantity) * int(quantity)
+        self.ingredients[ingredient] -= quantity
+        self.price -= quantity * price_per_quantity
 
     def make_order(self):
-        if not self.ordered:
-            all_ingredients = []
-            for key, value in self.ingredients.items():
-                all_ingredients.append(f"{key}: {value}")
-            self.ordered = True
-            return f"You've ordered pizza {self.name} prepared with {', '.join(all_ingredients)} and the price" \
-                   f" will be {int(self.price)}lv."
-        return f"Pizza {self.name} already prepared, and we can't make any changes!"
+        self.ordered = True
+        show_ingredients = [f"{key}: {str(value)}" for key, value in self.ingredients.items()]
+        return f"You've ordered pizza {self.name} prepared with {', '.join(show_ingredients)} and" \
+               f" the price will be {int(self.price)}lv."
+
+
+margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
+margarita.add_extra('mozzarella', 1, 0.5)
+margarita.add_extra('cheese', 1, 1)
+margarita.remove_ingredient('cheese', 1, 1)
+print(margarita.remove_ingredient('bacon', 1, 2.5))
+print(margarita.remove_ingredient('tomatoes', 2, 0.5))
+margarita.remove_ingredient('cheese', 2, 1)
+print(margarita.make_order())
+print(margarita.add_extra('cheese', 1, 1))
+
+
+# class PizzaDelivery:
+#
+#     def __init__(self, name, price: float, ingredients: dict):
+#         self.name = name
+#         self.price = float(price)
+#         self.ingredients = ingredients
+#         self.ordered = False
+#
+#     def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
+#         if self.ordered:
+#             return f"Pizza {self.name} already prepared, and we can't make any changes!"
+#         if ingredient in self.ingredients:
+#             self.ingredients[ingredient] += int(quantity)
+#             self.price += float(price_per_quantity) * int(quantity)
+#         elif ingredient not in self.ingredients:
+#             self.ingredients[ingredient] = int(quantity)
+#             self.price += int(quantity) * float(price_per_quantity)
+#
+#     def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
+#         if self.ordered:
+#             return f"Pizza {self.name} already prepared, and we can't make any changes!"
+#         if ingredient not in self.ingredients:
+#             return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+#         elif ingredient in self.ingredients and self.ingredients[ingredient] < quantity:
+#             return f"Please check again the desired quantity of {ingredient}!"
+#         self.ingredients[ingredient] -= int(quantity)
+#         self.price -= float(price_per_quantity) * int(quantity)
+#
+#     def make_order(self):
+#         if not self.ordered:
+#             all_ingredients = []
+#             for key, value in self.ingredients.items():
+#                 all_ingredients.append(f"{key}: {value}")
+#             self.ordered = True
+#             return f"You've ordered pizza {self.name} prepared with {', '.join(all_ingredients)} and the price" \
+#                    f" will be {int(self.price)}lv."
+#         return f"Pizza {self.name} already prepared, and we can't make any changes!"
--- Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/project_to_do_list/section.py
+++ Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/project_to_do_list/section.py
@@ -8,29 +8,80 @@ class Section:
         self.tasks = []
 
     def add_task(self, new_task: Task):
-        for info in self.tasks:
-            if new_task.name in info.name:
-                return f"Task is already in the section {self.name}"
+        if new_task in self.tasks:
+            return f"Task is already in the section {self.name}"
         self.tasks.append(new_task)
         return f"Task {new_task.details()} is added to the section"
 
     def complete_task(self, task_name: str):
-        for task_ in self.tasks:
-            if task_name == task_.name:
-                task_.completed = True
+        for item in self.tasks:
+            if item.name == task_name:
+                item.completed = True
                 return f"Completed task {task_name}"
         return f"Could not find task with the name {task_name}"
 
     def clean_section(self):
-        count = 0
-        for pos, tasks in enumerate(self.tasks):
-            if tasks.completed:
-                count += 1
-                del self.tasks[pos]
-        return f"Cleared {count} tasks."
+        cleared_tasks = 0
+        for item in range(len(self.tasks) - 1, -1, -1):
+            if self.tasks[item].completed:
+                cleared_tasks += 1
+                self.tasks.pop(item)
+        return f"Cleared {cleared_tasks} tasks."
 
     def view_section(self):
-        output = f"Section {self.name}:\n"
-        for info in self.tasks:
-            output += f"{info.details()}\n"
-        return output
+        output = [f"Section {self.name}:"]
+        for item in self.tasks:
+            output.append(item.details())
+        return '\n'.join(output)
+
+
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+print(section.clean_section())
+print(section.view_section())
+
+
+# from project.task import Task
+#
+#
+# class Section:
+#
+#     def __init__(self, name: str):
+#         self.name = name
+#         self.tasks = []
+#
+#     def add_task(self, new_task: Task):
+#         for info in self.tasks:
+#             if new_task.name in info.name:
+#                 return f"Task is already in the section {self.name}"
+#         self.tasks.append(new_task)
+#         return f"Task {new_task.details()} is added to the section"
+#
+#     def complete_task(self, task_name: str):
+#         for task_ in self.tasks:
+#             if task_name == task_.name:
+#                 task_.completed = True
+#                 return f"Completed task {task_name}"
+#         return f"Could not find task with the name {task_name}"
+#
+#     def clean_section(self):
+#         count = 0
+#         for pos, tasks in enumerate(self.tasks):
+#             if tasks.completed:
+#                 count += 1
+#                 del self.tasks[pos]
+#         return f"Cleared {count} tasks."
+#
+#     def view_section(self):
+#         output = f"Section {self.name}:\n"
+#         for info in self.tasks:
+#             output += f"{info.details()}\n"
+#         return output
