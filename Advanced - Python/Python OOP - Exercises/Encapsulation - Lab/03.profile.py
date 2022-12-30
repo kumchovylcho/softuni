b774ea8dc396f27b59a1dea00860e55222cd5594
kumchovylcho <tamerchoyy@gmail.com>
2022-12-31 00:11:20 +0200

    ref

--- Advanced - Python/Python OOP - Exercises/Encapsulation - Lab/03.profile.py
+++ Advanced - Python/Python OOP - Exercises/Encapsulation - Lab/03.profile.py
@@ -9,9 +9,9 @@ class Profile:
         return self.__username
 
     @username.setter
-    def username(self, current_name):
-        if 5 <= len(current_name) <= 15:
-            self.__username = current_name
+    def username(self, current_username):
+        if 5 <= len(current_username) <= 15:
+            self.__username = current_username
         else:
             raise ValueError("The username must be between 5 and 15 characters.")
 
@@ -21,18 +21,52 @@ class Profile:
 
     @password.setter
     def password(self, current_password):
-        if len(current_password) >= 8:
-            uppercase, digit = 0, 0
-            for symbol in current_password:
-                if symbol.isupper():
-                    uppercase += 1
-                elif symbol.isdigit():
-                    digit += 1
-            if uppercase and digit:
-                self.__password = current_password
-        if len(current_password) < 8 or not sum(1 for x in current_password if x.isupper()) or \
-                not sum(1 for x in current_password if x.isdigit()):
+        if len(current_password) >= 8 and \
+                [1 for x in current_password if x.isupper()] and \
+                [1 for x in current_password if x.isdigit()]:
+            self.__password = current_password
+        else:
             raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
 
     def __str__(self):
-        return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
+
+
+# class Profile:
+#
+#     def __init__(self, username: str, password: str):
+#         self.username = username
+#         self.password = password
+#
+#     @property
+#     def username(self):
+#         return self.__username
+#
+#     @username.setter
+#     def username(self, current_name):
+#         if 5 <= len(current_name) <= 15:
+#             self.__username = current_name
+#         else:
+#             raise ValueError("The username must be between 5 and 15 characters.")
+#
+#     @property
+#     def password(self):
+#         return self.__password
+#
+#     @password.setter
+#     def password(self, current_password):
+#         if len(current_password) >= 8:
+#             uppercase, digit = 0, 0
+#             for symbol in current_password:
+#                 if symbol.isupper():
+#                     uppercase += 1
+#                 elif symbol.isdigit():
+#                     digit += 1
+#             if uppercase and digit:
+#                 self.__password = current_password
+#         if len(current_password) < 8 or not sum(1 for x in current_password if x.isupper()) or \
+#                 not sum(1 for x in current_password if x.isdigit()):
+#             raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+#
+#     def __str__(self):
+#         return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'
--- Advanced - Python/Python OOP - Exercises/Encapsulation - Lab/04.email_validator.py
+++ Advanced - Python/Python OOP - Exercises/Encapsulation - Lab/04.email_validator.py
@@ -1,29 +1,53 @@
 class EmailValidator:
 
-    def __init__(self, min_length, mails, domains):
+    def __init__(self, min_length: int, mails: list, domains: list):
         self.min_length = min_length
         self.mails = mails
         self.domains = domains
 
     def __is_name_valid(self, name):
-        if len(name) >= self.min_length:
-            return True
-        return False
+        return len(name) >= self.min_length
 
     def __is_mail_valid(self, mail):
-        if mail in self.mails:
-            return True
-        return False
+        return mail in self.mails
 
     def __is_domain_valid(self, domain):
-        if domain in self.domains:
+        return domain in self.domains
+
+    def validate(self, email):
+        name, mail_and_domain = email.split("@")
+        current_mail, domain = mail_and_domain.split(".")
+        if self.__is_name_valid(name) and self.__is_mail_valid(current_mail) and self.__is_domain_valid(domain):
             return True
         return False
 
-    def validate(self, email):
-        email = email.split("@")
-        get_domain = email[-1].split(".")
-        name, mail, domain = email[0], get_domain[0], get_domain[-1]
-        if self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain):
-            return True
-        return False
+
+# class EmailValidator:
+#
+#     def __init__(self, min_length, mails, domains):
+#         self.min_length = min_length
+#         self.mails = mails
+#         self.domains = domains
+#
+#     def __is_name_valid(self, name):
+#         if len(name) >= self.min_length:
+#             return True
+#         return False
+#
+#     def __is_mail_valid(self, mail):
+#         if mail in self.mails:
+#             return True
+#         return False
+#
+#     def __is_domain_valid(self, domain):
+#         if domain in self.domains:
+#             return True
+#         return False
+#
+#     def validate(self, email):
+#         email = email.split("@")
+#         get_domain = email[-1].split(".")
+#         name, mail, domain = email[0], get_domain[0], get_domain[-1]
+#         if self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain):
+#             return True
+#         return False
