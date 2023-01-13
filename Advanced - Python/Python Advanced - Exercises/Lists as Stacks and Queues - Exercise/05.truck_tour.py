5d46c03ea85ba8ffc1b52f4e52d6ab92a5f58074
kumchovylcho <tamerchoyy@gmail.com>
2023-01-13 17:05:47 +0200

    new solutions

--- Advanced - Python/Python Advanced - Exercises/Lists as Stacks and Queues - Exercise/05.truck_tour.py
+++ Advanced - Python/Python Advanced - Exercises/Lists as Stacks and Queues - Exercise/05.truck_tour.py
@@ -1,24 +1,48 @@
 from collections import deque
-queue = deque()
-pumps = int(input())
-for _ in range(pumps):
-    gas_pump = input().split()
-    queue.append([int(gas_pump[0]), int(gas_pump[1])])
 
-for attempt in range(pumps):
-    fuel_tank = 0
-    pumps_travelled = 0
-    for gas_pump in queue:
-        fuel, distance_to_next_pump = gas_pump[0], gas_pump[1]
-        fuel_tank += fuel
-        if fuel_tank < distance_to_next_pump:
+number_pumps = int(input())
+
+circle = deque()
+for _ in range(number_pumps):
+    amount_petrol, distance = [int(x) for x in input().split()]
+    circle.append((amount_petrol, distance))
+
+for i in range(len(circle)):
+    current_circle = circle.copy()
+    saved_petrol = 0
+    while current_circle:
+        drive = current_circle.popleft()
+        if drive[0] + saved_petrol >= drive[1]:
+            saved_petrol += drive[0] - drive[1]
+        else:
             break
-        fuel_tank -= distance_to_next_pump
-        pumps_travelled += 1
-    if pumps_travelled == len(queue):
-        print(attempt)
+    if len(current_circle) == 0:
+        print(i)
         break
+    circle.rotate(-1)
 
-    queue.rotate(-1)
+
+# from collections import deque
+# queue = deque()
+# pumps = int(input())
+# for _ in range(pumps):
+#     gas_pump = input().split()
+#     queue.append([int(gas_pump[0]), int(gas_pump[1])])
+#
+# for attempt in range(pumps):
+#     fuel_tank = 0
+#     pumps_travelled = 0
+#     for gas_pump in queue:
+#         fuel, distance_to_next_pump = gas_pump[0], gas_pump[1]
+#         fuel_tank += fuel
+#         if fuel_tank < distance_to_next_pump:
+#             break
+#         fuel_tank -= distance_to_next_pump
+#         pumps_travelled += 1
+#     if pumps_travelled == len(queue):
+#         print(attempt)
+#         break
+#
+#     queue.rotate(-1)
 
 
--- Advanced - Python/Python Advanced - Exercises/Lists as Stacks and Queues - Exercise/06.balanced_parentheses.py
+++ Advanced - Python/Python Advanced - Exercises/Lists as Stacks and Queues - Exercise/06.balanced_parentheses.py
@@ -1,24 +1,43 @@
 from collections import deque
 
 parentheses = deque(input())
-open_per = deque()
-balanced = True
+check = []
 while parentheses:
-    check_par = parentheses.popleft()
-    if check_par in "{[(":
-        open_per.append(check_par)
-    elif not open_per:
-        balanced = False
-        break
-    else:
-        check_last_par = open_per.pop()
-        test_pair = f"{check_last_par + check_par}"
-        if test_pair not in "{}()[]":
-            balanced = False
+    current = parentheses.popleft()
+    if current in "{[(":
+        check.append(current)
+    elif current in "}])" and check:
+        if check.pop() + current not in "()[]{}":
+            print("NO")
             break
-
-if balanced and not open_per:
-    print("YES")
+    else:
+        print("NO")
+        break
 else:
-    print("NO")
+    print("YES")
+
+
+# from collections import deque
+#
+# parentheses = deque(input())
+# open_per = deque()
+# balanced = True
+# while parentheses:
+#     check_par = parentheses.popleft()
+#     if check_par in "{[(":
+#         open_per.append(check_par)
+#     elif not open_per:
+#         balanced = False
+#         break
+#     else:
+#         check_last_par = open_per.pop()
+#         test_pair = f"{check_last_par + check_par}"
+#         if test_pair not in "{}()[]":
+#             balanced = False
+#             break
+#
+# if balanced and not open_per:
+#     print("YES")
+# else:
+#     print("NO")
 
