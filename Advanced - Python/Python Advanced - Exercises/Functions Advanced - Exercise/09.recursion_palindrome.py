9401878355604a26499ec125e87299e51d9d8af8
kumchovylcho <tamerchoyy@gmail.com>
2023-02-01 23:39:12 +0200

    ref

--- Advanced - Python/Python Advanced - Exercises/Functions Advanced - Exercise/09.recursion_palindrome.py
+++ Advanced - Python/Python Advanced - Exercises/Functions Advanced - Exercise/09.recursion_palindrome.py
@@ -1,4 +1,14 @@
-def palindrome(word, index):
-    if word == word[::-1]:
+def palindrome(word, index, right_index=-1):
+    if index == len(word) // 2:
         return f"{word} is a palindrome"
-    return f"{word} is not a palindrome"
+
+    if word[index] != word[right_index]:
+        return f"{word} is not a palindrome"
+
+    return palindrome(word, index + 1, right_index - 1)
+
+
+# def palindrome(word, index):
+#     if word == word[::-1]:
+#         return f"{word} is a palindrome"
+#     return f"{word} is not a palindrome"
