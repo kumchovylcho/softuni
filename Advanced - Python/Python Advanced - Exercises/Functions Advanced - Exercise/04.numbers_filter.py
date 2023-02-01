39a22a24ecbae9bada77926f15b97d9f52911002
kumchovylcho <tamerchoyy@gmail.com>
2023-02-01 22:57:48 +0200

    ref

--- Advanced - Python/Python Advanced - Exercises/Functions Advanced - Exercise/04.numbers_filter.py
+++ Advanced - Python/Python Advanced - Exercises/Functions Advanced - Exercise/04.numbers_filter.py
@@ -1,15 +1,25 @@
 def even_odd_filter(**kwargs):
-    filtered = {}
-    for command in kwargs:
-        filtered[command] = filtered.get(command, [])
-        if command == 'even':
-            filtered[command] = [digit for digit in kwargs[command] if digit % 2 == 0]
-        elif command == 'odd':
-            filtered[command] = [digit for digit in kwargs[command] if digit % 2 != 0]
-    sorted_items = {}
-    for operation, values in sorted(filtered.items(), key=lambda item: -len(item[1])):
-        sorted_items[operation] = values
-    return sorted_items
+    for key, value in kwargs.items():
+        if key == "odd":
+            kwargs[key] = [x for x in value if x % 2 != 0]
+        elif key == "even":
+            kwargs[key] = [x for x in value if x % 2 == 0]
+
+    return dict(sorted(kwargs.items(), key=lambda x: -len(x[1])))
+
+
+# def even_odd_filter(**kwargs):
+#     filtered = {}
+#     for command in kwargs:
+#         filtered[command] = filtered.get(command, [])
+#         if command == 'even':
+#             filtered[command] = [digit for digit in kwargs[command] if digit % 2 == 0]
+#         elif command == 'odd':
+#             filtered[command] = [digit for digit in kwargs[command] if digit % 2 != 0]
+#     sorted_items = {}
+#     for operation, values in sorted(filtered.items(), key=lambda item: -len(item[1])):
+#         sorted_items[operation] = values
+#     return sorted_items
 
 
 print(even_odd_filter(
