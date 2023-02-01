e0f1029b92c3995c6a1d689ec8dae44d9135ee46
kumchovylcho <tamerchoyy@gmail.com>
2023-02-01 22:49:36 +0200

    ref

--- Advanced - Python/Python Advanced - Exercises/Functions Advanced - Exercise/03.even_or_odd.py
+++ Advanced - Python/Python Advanced - Exercises/Functions Advanced - Exercise/03.even_or_odd.py
@@ -1,6 +1,15 @@
 def even_odd(*args):
     command = args[-1]
-    if command == "even":
-        return [digit for digit in args[:-1] if digit % 2 == 0]
-    elif command == "odd":
-        return [digit for digit in args[:-1] if digit % 2 != 0]
+    operations = {
+        "even": lambda numbers: [x for x in numbers[:-1] if int(x) % 2 == 0],
+        "odd": lambda numbers: [x for x in numbers[:-1] if int(x) % 2 != 0]
+    }
+    return operations[command](args)
+
+
+# def even_odd(*args):
+#     command = args[-1]
+#     if command == "even":
+#         return [digit for digit in args[:-1] if digit % 2 == 0]
+#     elif command == "odd":
+#         return [digit for digit in args[:-1] if digit % 2 != 0]
