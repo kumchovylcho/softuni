6f63b4475bbd790cef4eee7c405ee02bf4c4d446
kumchovylcho <tamerchoyy@gmail.com>
2023-02-11 01:27:14 +0200

    added solution with lambdas

--- Fundamentals - Python/Functions - lab/Calculations.py
+++ Fundamentals - Python/Functions - lab/Calculations.py
@@ -1,19 +1,32 @@
-operation = input()
-first_number = int(input())
-second_number = int(input())
+calculator_executor = {
+    "multiply": lambda a, b: a * b,
+    "divide": lambda a, b: int(a / b),
+    "add": lambda a, b: a + b,
+    "subtract": lambda a, b: a - b
+}
+
+current_operator = input()
+current_a = int(input())
+current_b = int(input())
+
+print(calculator_executor[current_operator](current_a, current_b))
 
 
-def calculations(number_one, number_two, operator):
-    result = None
-    if operator == "multiply":
-        result = number_one * number_two
-    elif operator == "divide":
-        result = int(number_one / number_two)
-    elif operator == "add":
-        result = number_one + number_two
-    elif operator == "subtract":
-        result = number_one - number_two
-    return result
-
-
-print(calculations(first_number, second_number, operation))
+# def calculations(number_one, number_two, operator):
+#     result = None
+#     if operator == "multiply":
+#         result = number_one * number_two
+#     elif operator == "divide":
+#         result = int(number_one / number_two)
+#     elif operator == "add":
+#         result = number_one + number_two
+#     elif operator == "subtract":
+#         result = number_one - number_two
+#     return result
+#
+#
+# operation = input()
+# first_number = int(input())
+# second_number = int(input())
+#
+# print(calculations(first_number, second_number, operation))
