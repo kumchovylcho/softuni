384fef4fc1ac53ee0a905f3eaa4345dbc43139df
kumchovylcho <tamerchoyy@gmail.com>
2023-02-14 23:09:40 +0200

    ref

--- Programming Basics - Python/Conditional_Statements_Advanced_Lab/Day_of_week.py
+++ Programming Basics - Python/Conditional_Statements_Advanced_Lab/Day_of_week.py
@@ -15,3 +15,18 @@ elif day_of_week == 7:
     print("Sunday")
 else:
     print("Error")
+
+
+# day_of_week = int(input())
+#
+# days_of_week = {
+#     1: "Monday",
+#     2: "Tuesday",
+#     3: "Wednesday",
+#     4: "Thursday",
+#     5: "Friday",
+#     6: "Saturday",
+#     7: "Sunday",
+# }
+#
+# print(days_of_week.get(day_of_week, "Error"))
