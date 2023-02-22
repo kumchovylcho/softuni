803439f307c4fab96ca8cb8f20e345832b3a54e2
kumchovylcho <tamerchoyy@gmail.com>
2023-02-22 16:40:31 +0200

    ref

---
+++ Fundamentals - Python/Dictionaries - exercise/06.orders.py
@@ -0,0 +1,58 @@
+storage = {}
+
+products = input()
+while products != "buy":
+    product_name, product_price, product_quantity = [float(item) if item[-1].isdigit() else item for item in products.split()]
+
+    storage[product_name] = storage.get(product_name, {"price": 0, "quantity": 0})
+    storage[product_name]['quantity'] += product_quantity
+    storage[product_name]['price'] = product_price
+
+    products = input()
+
+for key, value in storage.items():
+    print(f"{key} -> {value['price'] * value['quantity']:.2f}")
+
+
+# storage = dict()
+#
+# products = input()
+# while products != "buy":
+#     products = products.split()
+#
+#     product_name, product_price, product_quantity = [float(item) if item[-1].isdigit() else item for item in products]
+#     storage[product_name] = storage.get(product_name, {"price": product_price, "quantity": product_quantity})
+#
+#     if storage[product_name]["price"] != product_price:
+#         storage[product_name]["price"] = product_price
+#         storage[product_name]["quantity"] += product_quantity
+#     products = input()
+#
+# for key, values in storage.items():
+#     print(f"{key} -> {storage[key]['quantity'] * storage[key]['price']:.2f} ")
+
+
+# orders = dict()
+#
+#
+# products = input()
+# while products != "buy":
+#     products = products.split()
+#
+#     product = products[0]
+#     price = float(products[1])
+#     quantity = int(products[2])
+#
+#     if product not in orders:
+#         orders[product] = [price, quantity]
+#
+#     elif product in orders:
+#         orders[product][1] += quantity
+#         if price != orders[product][0]:
+#             orders[product][0] = price
+#
+#     products = input()
+#
+# for product in orders:
+#     total_price = orders[product][0] * orders[product][1]
+#     print(f"{product} -> {total_price:.2f}")
