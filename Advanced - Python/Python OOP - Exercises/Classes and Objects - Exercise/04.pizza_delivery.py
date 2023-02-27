d94ea58ff072f6cccf348a97e48baff2bb8eef5d
kumchovylcho <tamerchoyy@gmail.com>
2023-02-27 23:01:11 +0200

    ref

--- Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/04.pizza_delivery.py
+++ Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/04.pizza_delivery.py
@@ -2,35 +2,35 @@ class PizzaDelivery:
 
     def __init__(self, name: str, price: float, ingredients: dict):
         self.name = name
-        self.price = float(price)
+        self.price = price
         self.ingredients = ingredients
         self.ordered = False
 
     def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
         if self.ordered:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
         self.ingredients[ingredient] = self.ingredients.get(ingredient, 0) + quantity
-        # if ingredient in self.ingredients:
-        #     self.ingredients[ingredient] += quantity
-        # elif ingredient not in self.ingredients:
-        #     self.ingredients[ingredient] = quantity
         self.price += quantity * price_per_quantity
 
     def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
         if self.ordered:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
         if ingredient not in self.ingredients:
             return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
-        if self.ingredients[ingredient] < quantity:
+
+        if self.ingredients[ingredient] - quantity < 0:
             return f"Please check again the desired quantity of {ingredient}!"
+
         self.ingredients[ingredient] -= quantity
-        self.price -= quantity * price_per_quantity
+        self.price -= price_per_quantity * quantity
 
     def make_order(self):
         self.ordered = True
-        show_ingredients = [f"{key}: {str(value)}" for key, value in self.ingredients.items()]
-        return f"You've ordered pizza {self.name} prepared with {', '.join(show_ingredients)} and" \
-               f" the price will be {int(self.price)}lv."
+        ingredients = ', '.join(f"{key}: {value}" for key , value in self.ingredients.items())
+
+        return f"You've ordered pizza {self.name} prepared with {ingredients} and the price will be {self.price}lv."
 
 
 margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
@@ -42,6 +42,41 @@ print(margarita.remove_ingredient('tomatoes', 2, 0.5))
 margarita.remove_ingredient('cheese', 2, 1)
 print(margarita.make_order())
 print(margarita.add_extra('cheese', 1, 1))
+
+
+# class PizzaDelivery:
+#
+#     def __init__(self, name: str, price: float, ingredients: dict):
+#         self.name = name
+#         self.price = float(price)
+#         self.ingredients = ingredients
+#         self.ordered = False
+#
+#     def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
+#         if self.ordered:
+#             return f"Pizza {self.name} already prepared, and we can't make any changes!"
+#         self.ingredients[ingredient] = self.ingredients.get(ingredient, 0) + quantity
+#         # if ingredient in self.ingredients:
+#         #     self.ingredients[ingredient] += quantity
+#         # elif ingredient not in self.ingredients:
+#         #     self.ingredients[ingredient] = quantity
+#         self.price += quantity * price_per_quantity
+#
+#     def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
+#         if self.ordered:
+#             return f"Pizza {self.name} already prepared, and we can't make any changes!"
+#         if ingredient not in self.ingredients:
+#             return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+#         if self.ingredients[ingredient] < quantity:
+#             return f"Please check again the desired quantity of {ingredient}!"
+#         self.ingredients[ingredient] -= quantity
+#         self.price -= quantity * price_per_quantity
+#
+#     def make_order(self):
+#         self.ordered = True
+#         show_ingredients = [f"{key}: {str(value)}" for key, value in self.ingredients.items()]
+#         return f"You've ordered pizza {self.name} prepared with {', '.join(show_ingredients)} and" \
+#                f" the price will be {int(self.price)}lv."
 
 
 # class PizzaDelivery:
