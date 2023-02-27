32058c9f5ed642c3df49cf2e2e0627fee246a165
kumchovylcho <tamerchoyy@gmail.com>
2023-02-27 22:38:20 +0200

    ref

--- Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/03.account.py
+++ Advanced - Python/Python OOP - Exercises/Classes and Objects - Exercise/03.account.py
@@ -1,19 +1,41 @@
 class Account:
 
-    def __init__(self, id: int, name, balance=0):
-        self.id = int(id)
+    def __init__(self, id: int, name: str, balance=0):
+        self.id = id
         self.name = name
-        self.balance = int(balance)
+        self.balance = balance
 
-    def credit(self, amount: int):
+    def credit(self, amount: int or float):
         self.balance += amount
         return self.balance
 
-    def debit(self, amount: int):
-        if self.balance >= amount:
-            self.balance -= amount
-            return self.balance
-        return "Amount exceeded balance"
+    def debit(self, amount: int or float):
+        if amount > self.balance:
+            return "Amount exceeded balance"
+
+        self.balance -= amount
+        return self.balance
 
     def info(self):
         return f"User {self.name} with account {self.id} has {self.balance} balance"
+
+
+# class Account:
+#
+#     def __init__(self, id: int, name, balance=0):
+#         self.id = int(id)
+#         self.name = name
+#         self.balance = int(balance)
+#
+#     def credit(self, amount: int):
+#         self.balance += amount
+#         return self.balance
+#
+#     def debit(self, amount: int):
+#         if self.balance >= amount:
+#             self.balance -= amount
+#             return self.balance
+#         return "Amount exceeded balance"
+#
+#     def info(self):
+#         return f"User {self.name} with account {self.id} has {self.balance} balance"
