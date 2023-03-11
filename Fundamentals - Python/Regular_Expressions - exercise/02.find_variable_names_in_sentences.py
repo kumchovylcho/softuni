7c485f7f8e7aeb1010fab74f8a061e70feca0159
kumchovylcho <tamerchoyy@gmail.com>
2023-03-11 02:43:30 +0200

    added one liner solution

--- Fundamentals - Python/Regular_Expressions - exercise/02.find_variable_names_in_sentences.py
+++ Fundamentals - Python/Regular_Expressions - exercise/02.find_variable_names_in_sentences.py
@@ -4,3 +4,7 @@ pattern = re.compile(r'\b_(?P<valid>[A-Za-z\d]+\b)')
 matches = pattern.finditer(text)
 result = [match['valid'] for match in matches]
 print(*result, sep=',')
+
+
+# import re
+# print(','.join([match['valid'] for match in re.compile(r'\b_(?P<valid>[A-Za-z\d]+\b)').finditer(input())]))
