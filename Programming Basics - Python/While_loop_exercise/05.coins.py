b9d4dd562ac6c9b0d50c35312ccea7a41cfcb229
kumchovylcho <tamerchoyy@gmail.com>
2023-04-18 02:28:24 +0300

    ref

---
+++ Programming Basics - Python/While_loop_exercise/05.coins.py
@@ -0,0 +1,41 @@
+charge_of_money = float(input())
+coins_left = round(charge_of_money * 100)
+
+number_of_coins = 0
+
+while coins_left > 0:
+
+    for coin in (200, 100, 50, 20, 10, 5, 2, 1):
+        if coins_left - coin >= 0:
+            coins_left -= coin
+            number_of_coins += 1
+            break
+
+
+print(number_of_coins)
+
+
+
+
+# charge_of_money = float(input())
+# coins_left = round(charge_of_money * 100)
+# number_of_coins = 0
+# while coins_left > 0:
+#     if coins_left >= 200:
+#         coins_left -= 200
+#     elif coins_left >= 100:
+#         coins_left -= 100
+#     elif coins_left >= 50:
+#         coins_left -= 50
+#     elif coins_left >= 20:
+#         coins_left -= 20
+#     elif coins_left >= 10:
+#         coins_left -= 10
+#     elif coins_left >= 5:
+#         coins_left -= 5
+#     elif coins_left >= 2:
+#         coins_left -= 2
+#     elif coins_left >= 1:
+#         coins_left -= 1
+#     number_of_coins += 1
+# print(number_of_coins)
