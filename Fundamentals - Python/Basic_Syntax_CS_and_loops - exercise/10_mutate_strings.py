b2aa72701acca5fe84dafccbc5ad6b8b71b79b97
Tamer <tamerchoyy@gmail.com>
2023-08-20 01:08:22 +0300

    ref

---
+++ Fundamentals - Python/Basic_Syntax_CS_and_loops - exercise/10_mutate_strings.py
@@ -0,0 +1,33 @@
+first_string = input()
+second_string = input()
+
+for i in range(len(second_string)):
+    if first_string[i] == second_string[i]:
+        continue
+
+    result = second_string[:i + 1] + first_string[i + 1:]
+    print(result)
+
+
+
+
+# first_word, second_word = list(input()), list(input())
+# for index in range(len(first_word)):
+#     if first_word[index] != second_word[index]:
+#         first_word[index] = second_word[index]
+#         print(*first_word, sep='')
+
+
+# first_text = input()
+# second_text = input()
+# list_one = shuffled_cards(first_text)
+# list_two = shuffled_cards(second_text)
+# i = 0
+# target_list = shuffled_cards(first_text)
+# for letter_a, letter_b in zip(list_one, list_two):
+#     if letter_a != letter_b:
+#         target_list[i] = letter_b
+#         for _ in target_list:
+#             print(f"{_}", end="")
+#         print()
+#     i += 1
