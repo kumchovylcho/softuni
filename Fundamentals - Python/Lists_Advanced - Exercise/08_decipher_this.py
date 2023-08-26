66d38fe2bee9e3539e0fc291bed48724504de7b2
Tamer <tamerchoyy@gmail.com>
2023-08-26 22:56:56 +0300

    update with isdigit method

--- Fundamentals - Python/Lists_Advanced - Exercise/08_decipher_this.py
+++ Fundamentals - Python/Lists_Advanced - Exercise/08_decipher_this.py
@@ -5,7 +5,7 @@ for word in words:
     cur_word = list(word)
 
     char_code = []
-    while cur_word[0] in "0123456789":
+    while cur_word[0].isdigit():
         char_code.append(cur_word.pop(0))
 
     cur_word.insert(0, chr(int("".join(char_code))))
