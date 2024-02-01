20b9811366b72869ea39a609d9c2c2a8c37d1453
Tamer <tamerchoyy@gmail.com>
2024-02-01 02:52:03 +0200

    ref

--- Advanced - Python/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/02.crud.py
+++ Advanced - Python/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/02.crud.py
@@ -3,19 +3,20 @@ def create(row, col, replacement_value):
         matrix[row][col] = replacement_value
 
 
-def update(row, col, replacement_value):
+def update(row, col, replacement_value, show_square=False):
     if matrix[row][col] != ".":
-        matrix[row][col] = replacement_value
+        if show_square:
+            print(matrix[row][col])
+        elif not show_square:
+            matrix[row][col] = replacement_value
 
 
 def delete(row, col):
-    if matrix[row][col] != ".":
-        matrix[row][col] = "."
+    update(row, col, ".")
 
 
 def read(row, col):
-    if matrix[row][col] != ".":
-        print(matrix[row][col])
+    update(row, col, "", show_square=True)
 
 
 def show_result():
@@ -25,19 +26,28 @@ def show_result():
 matrix_size = 6
 matrix = [[col for col in input().split()] for row in range(matrix_size)]
 start_row, start_col = [int(x) for x in input() if x.isdigit()]
-start_position = [start_row, start_col]
+
 operations = {
     'Create': create,
     'Update': update,
     'Delete': delete,
     'Read': read
 }
+
+movement = {
+    "up": (-1, 0),
+    "down": (1, 0),
+    "right": (0, 1),
+    "left": (0, -1),
+}
+
 command = input()
 while command != "Stop":
     operation, direction, *value = [x for x in command.split(", ")]
-    for where, row, col in ('up', -1, 0), ('down', 1, 0), ('right', 0, 1), ('left', 0, -1):
-        if where == direction:
-            start_position = start_position[0] + row, start_position[1] + col
-            operations[operation](*start_position, *value)
+
+    start_row, start_col = start_row + movement[direction][0], start_col + movement[direction][1]
+    operations[operation](start_row, start_col, *value)
+
     command = input()
+
 show_result()
