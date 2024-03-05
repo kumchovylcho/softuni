53c25beaaaddf4288ac8117da70df7ba436c01c3
Tamer <tamerchoyy@gmail.com>
2024-03-06 01:46:31 +0200

    added abstraction

--- Advanced - Python/Python OOP - Exercises/Static and Class Methods - Exercise/project_document_management/storage.py
+++ Advanced - Python/Python OOP - Exercises/Static and Class Methods - Exercise/project_document_management/storage.py
@@ -10,51 +10,58 @@ class Storage:
         self.topics = []
         self.documents = []
 
+    @staticmethod
+    def add_object(obj, collection):
+        if obj not in collection:
+            collection.append(obj)
+
+    @staticmethod
+    def get_object(item, attribute: str, collection):
+        for obj in collection:
+            if getattr(obj, attribute) == item:
+                return obj
+
+    def delete_object(self, item, attribute, collection):
+        obj = self.get_object(item, attribute, collection)
+        if obj:
+            collection.remove(obj)
+
     def add_category(self, category: Category):
-        if category not in self.categories:
-            self.categories.append(category)
+        self.add_object(category, self.categories)
 
     def add_topic(self, topic: Topic):
-        if topic not in self.topics:
-            self.topics.append(topic)
+        self.add_object(topic, self.topics)
 
     def add_document(self, document: Document):
-        if document not in self.documents:
-            self.documents.append(document)
+        self.add_object(document, self.documents)
 
     def edit_category(self, category_id: int, new_name: str):
-        for category in self.categories:
-            if category.id == category_id:
-                category.name = new_name
+        category = self.get_object(category_id, "id", self.categories)
+        if category:
+            category.name = new_name
 
     def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
-        for topic in self.topics:
-            if topic.id == topic_id:
-                topic.topic = new_topic
-                topic.storage_folder = new_storage_folder
+        topic = self.get_object(topic_id, "id", self.topics)
+        if topic:
+            topic.topic = new_topic
+            topic.storage_folder = new_storage_folder
 
     def edit_document(self, document_id: int, new_file_name: str):
-        for document in self.documents:
-            if document.id == document_id:
-                document.file_name = new_file_name
+        document = self.get_object(document_id, "id", self.documents)
+        if document:
+            document.file_name = new_file_name
 
     def delete_category(self, category_id):
-        for category in self.categories:
-            if category.id == category_id:
-                self.categories.remove(category)
+        self.delete_object(category_id, "id", self.categories)
 
     def delete_topic(self, topic_id):
-        for topic in self.topics:
-            if topic.id == topic_id:
-                self.topics.remove(topic)
+        self.delete_object(topic_id, "id", self.topics)
 
     def delete_document(self, document_id):
-        for document in self.documents:
-            if document.id == document_id:
-                self.documents.remove(document)
+        self.delete_object(document_id, "id", self.documents)
 
     def get_document(self, document_id):
-        document = [d for d in self.documents if d.id == document_id]
+        document = self.get_object(document_id, "id", self.documents)
         if document:
             return document[0]
 
